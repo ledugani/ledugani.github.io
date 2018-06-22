15cb61f4eb665d9b88c64dd612ec6d3b8db8fd1b
Tom Dugan <thomas@aadwebb.com>
2018-06-21 22:08:03 -0500

    wrote read fb fx

--- javascripts/firebaseApi.js
+++ javascripts/firebaseApi.js
@@ -9,7 +9,30 @@ const getConfig = () => {
   return fireBaseConfig;
 };
 
+const getAllBlogs = () => {
+  return new Promise((resolve, reject) => {
+    const allBlogsArray = [];
+    $.ajax({
+      method: 'GET',
+      url: 'https://personal-site-128be.firebaseio.com/blogs.json',
+    })
+      .done((allBlogsObj) => {
+        if (allBlogsObj !== null) {
+          Object.keys(allBlogsObj).forEach((fbkey) => {
+            allBlogsObj[fbkey].id = fbkey;
+            allBlogsArray.push(allBlogsObj[fbkey]);
+          });
+        }
+        resolve(allBlogsArray);
+      })
+      .fail((errrorrr) => {
+        reject(errrorrr);
+      });
+  });
+};
+
 module.exports = {
   setConfig,
   getConfig,
+  getAllBlogs,
 };
